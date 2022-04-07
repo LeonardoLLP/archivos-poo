6e2e4f8e97137aee878b9ea5854571d6a9d3be3e
Leonardo Luque <64507345+LeonardoLLP@users.noreply.github.com>
2022-04-08 00:12:58 +0200

    Finalizada función para añadir nota final

--- archivos.py
+++ archivos.py
@@ -5,7 +5,8 @@ data = pd.read_csv(filepath, sep=";")
 
 print(data.head())
 
-def alumni_info(data: pd.DataFrame):
+# Hemos considerado que la información que se pide es toda la que se halla en el csv, no he comprendido bien si querían que se suprimiese alguna
+def alumni_info(data: pd.DataFrame) -> list:
     my_list = []
     for index in range(len(data.index)):
         values = data.iloc[index]
@@ -17,5 +18,15 @@ def alumni_info(data: pd.DataFrame):
     return my_list
 
 my_alumni_info = alumni_info(data)
+
+
+
+def final_mark(data: list) -> list:
+    """Return the same data with final mark"""
+    for row in data:
+        row["NotaFinal"] = (row["Parcial1"] + row["Parcial2"]) * 0.3 + row["OrdinarioPracticas"] * 0.4
+
+
+
 
 
