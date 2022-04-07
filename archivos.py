30d0079e994409c322888694f67205dfd1b00f26
Leonardo Luque <64507345+LeonardoLLP@users.noreply.github.com>
2022-04-08 00:26:52 +0200

    This is stupid, still not working

--- archivos.py
+++ archivos.py
@@ -2,15 +2,19 @@ import pandas as pd
 from numpy import float64 as fl
 
 filepath = "calificaciones.csv"
-data = pd.read_csv(filepath, sep=";", dtype={
-    "Parcial1": fl,
-    "Parcial2": fl,
-    "Parcial3": fl,
-})
+data = pd.read_csv(filepath, sep=";")
 
 print(data.head())
 
-# Hemos considerado que la información que se pide es toda la que se halla en el csv, no he comprendido bien si querían que se suprimiese alguna
+# Simple function to attain to conditions of the csv
+def to_float(string: str):
+    string.replace(",", ".")
+    string.replace("Nan", "0")
+    return float(string)
+
+
+# Hemos considerado que la información que se pide a pasar a la lista es toda la que se halla en el csv,
+# no he comprendido bien si querían que se suprimiese alguna
 def alumni_info(data: pd.DataFrame) -> list:
     my_list = []
     for index in range(len(data.index)):
@@ -25,11 +29,12 @@ def alumni_info(data: pd.DataFrame) -> list:
 my_alumni_info = alumni_info(data)
 
 
-
 def add_final_mark(data: list) -> list:
     """Return the same data with final mark"""
     for row in data:
-        row["NotaFinal"] = (float(row["Parcial1"]) + float(row["Parcial2"])) * 0.3 + float(row["OrdinarioPracticas"]) * 0.4
+        row["NotaFinal"] = (to_float(row["Parcial1"]) + to_float(row["Parcial2"])) * 0.3 + to_float(row["OrdinarioPracticas"]) * 0.4
+
+
 
 add_final_mark(my_alumni_info)
 row = my_alumni_info[0]
