3b9c33772f8dff0db0f5e9265ede26f00fb79ef3
Leonardo Luque <64507345+LeonardoLLP@users.noreply.github.com>
2022-04-08 10:00:12 +0200

    Version with debugging code

--- archivos.py
+++ archivos.py
@@ -2,15 +2,17 @@ import pandas as pd
 from numpy import float64 as fl
 
 filepath = "calificaciones.csv"
-data = pd.read_csv(filepath, sep=";")
+cal_data = pd.read_csv(filepath, sep=";")
 
-print(data.head())
+print(cal_data.head())
 
 # Simple function to attain to conditions of the csv
 def to_float(value):
     str_value = str(value)
     final_string = str_value.replace(",", ".").replace("Nan", "0")
-    return float(final_string)
+    final_float = float(final_string)
+    print(type(final_float))
+    return float(final_float)
 
 
 # Hemos considerado que la información que se pide a pasar a la lista es toda la que se halla en el csv,
@@ -26,7 +28,7 @@ def alumni_info(data: pd.DataFrame) -> list:
 
     return my_list
 
-my_alumni_info = alumni_info(data)
+my_alumni_info = alumni_info(cal_data)
 
 #! NO FUNCIONA
 def add_final_mark(data: list):
@@ -34,10 +36,15 @@ def add_final_mark(data: list):
     for row in data:
         row["NotaFinal"] = (to_float(row["Parcial1"]) + to_float(row["Parcial2"])) * 0.3 + to_float(row["OrdinarioPracticas"]) * 0.4 #! REVISAR ESTO
 
+a = my_alumni_info
+tf = to_float
+
+# print(tf(a["Parcial1"]) + tf(a["Parcial2"]))
+
 
 #! NO FUNCIONA
 add_final_mark(my_alumni_info)
 
-for i in my_alumni_info:
-    print(i)
+# for i in my_alumni_info:
+#     print(i)
 
